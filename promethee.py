c4e7eea8ba81019a60cf1700b1f7553e99e84877
JULLIEN Maxime <maxime.jullien2@alumni.univ-avignon.fr>
2024-10-02 13:50:09 +0200

    Ajout d'un test pour le cas ou les données sont à None

--- promethee.py
+++ promethee.py
@@ -14,13 +14,19 @@ def promethee(selection_mode, data, array_type_operation):
             ValueError: 
                 - If selection_mode > 2 ou <= 0.
                 - If on indice on array-type_operation is different of "min" and "max".
+                - If data is None.
     """
     try:
         if selection_mode > 2 or selection_mode <= 0 : 
             raise ValueError("Type of Promethee need to be 1 or 2")
+        
         for i in range(len(array_type_operation)):
             if array_type_operation[i] != "max" and array_type_operation[i] != "min":
                 raise ValueError(f"At indice {i} , the array array_type_operation have a data different from 'min' or 'max'.")
+        
+        if data == None: 
+            raise ValueError("Data value is None")
+        
     except ValueError as err :
         print(err.args[0])
         exit(0)
