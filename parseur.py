580ad6293fdb19f0fa765c183521274cd1961d38
Florian Lambert <flo2009@laposte.net>
2024-10-10 17:21:46 +0200

    Dernier push 💤💤

--- parseur.py
+++ parseur.py
@@ -132,5 +132,5 @@ if __name__ == '__main__':
     countty = 0
     for y in table["TrueWeight"].tolist():
         countty = countty + y
-    print("total true weight= " + countty)
+    print("total true weight= ", countty)
     print(table["TrueWeight"].tolist())
