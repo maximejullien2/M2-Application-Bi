d263d5caf77bbcc3b3d0c246f1c48dea49f48e62
Jullien <maxime.jullien2@alumni.univ-avignon.fr>
2024-12-08 10:49:57 +0100

    Suppression des fichiers test.

--- weighted_sum_test.py
+++
@@ -1,7 +0,0 @@
-import weighted_sum
-import pandas as pd
-if __name__ == '__main__':
-    table = pd.read_csv('data/weighted_sum.csv')
-    print("Résultat de la somme pondérée : ")
-    print("A obtenir : [0 2 1 3] , Obtenu : ",weighted_sum.weighted_sum(table,["max","max"],"max"))
-
